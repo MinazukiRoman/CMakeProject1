f396ded59262250337a06f25a73a3f320b7eb7e3
roman_seredniov <serednv@mail.ru>
2024-11-29 22:07:42 +0300

    10.9

--- CMakeProject1/Test 1 .cpp
+++ CMakeProject1/Test 1 .cpp
@@ -1,76 +1,32 @@
-#include<iostream>
-#include<math.h>
-
-int main() {
-    using namespace std;
-
-    string departureTime, arrivalTime;
-
-    bool checkingForErrors;
-
-    int departureHours;
-    int departureMinutes;
-    int arrivalHours;
-    int arrivalMinutes;
-
-    do {
-        checkingForErrors = true;
-        cout << "Input departure time (HH:MM): ";
-        cin >> departureTime;
-
-        if (departureTime.length() != 5 || departureTime[2] != ':'
-            || departureTime[0] > '2' || departureTime[0] < '0'
-            || departureTime[1] > '9' || departureTime[1] < '0'
-            || departureTime[3] > '5' || departureTime[3] < '0'
-            || departureTime[4] > '9' || departureTime[4] < '0') {
-            checkingForErrors = false;
-        }
-        else {
-            departureHours = 10 * (departureTime[0] - '0') + (departureTime[1] - '0');
-            departureMinutes = 10 * (departureTime[3] - '0') + (departureTime[4] - '0');
-            if (departureHours > 23 || departureMinutes > 59) {
-                checkingForErrors = false;
-            }
-        }
-    } while (checkingForErrors == false);
-
-    do {
-        checkingForErrors = true;
-        cout << "Input arrival time (HH:MM): ";
-        cin >> arrivalTime;
-
-        if (arrivalTime.length() != 5 || arrivalTime[2] != ':'
-            || arrivalTime[0] > '2' || arrivalTime[0] < '0'
-            || arrivalTime[1] > '9' || arrivalTime[1] < '0'
-            || arrivalTime[3] > '5' || arrivalTime[3] < '0'
-            || arrivalTime[4] > '9' || arrivalTime[4] < '0') {
-            checkingForErrors = false;
-        }
-        else {
-            arrivalHours = 10 * (arrivalTime[0] - '0') + (arrivalTime[1] - '0');
-            arrivalMinutes = 10 * (arrivalTime[3] - '0') + (arrivalTime[4] - '0');
-            if (arrivalHours > 23 || arrivalMinutes > 59) {
-                checkingForErrors = false;
-            }
-        }
-    } while (checkingForErrors == false);
-
-    int hoursOnTheWay = 0;
-    int minutesOnTheWay = 0;
-
-    int differenceInMinutes = arrivalMinutes - departureMinutes;
-    int differenceInHourse = arrivalHours * 60 - departureHours * 60;
-    int allDifferenceInMinutes = differenceInMinutes + differenceInHourse;
-
-    hoursOnTheWay = differenceInHourse <= 0 ? 24 - abs(differenceInHourse / 60) : differenceInHourse / 60;
-    minutesOnTheWay = differenceInMinutes < 0 ? 60 - abs(differenceInMinutes) : differenceInMinutes;
-    if (hoursOnTheWay == 24) {
-        hoursOnTheWay = differenceInMinutes < 0 ? 23 : 0;
-    }if (differenceInMinutes < 0 && differenceInHourse > 0) {
-        hoursOnTheWay -= 1;
-    }if (differenceInMinutes < 0 && differenceInHourse < 0) {
-        hoursOnTheWay -= 1;
+#include <iostream>
+#include <cmath>
+int main()
+{
+    float a, b, c;
+    std::cout << "a, b, c:";
+    std::cin >> a >> b >> c;
+    if (a < 0)
+    {
+        std::cout << "Not a quadratic equation!" << std::endl;
     }
+    else if (b > 0)
+    {
+        float discriminant = b * b - 4 * a * c;
 
-    cout << "The trip took: " << hoursOnTheWay << " hours " << minutesOnTheWay << " minutes.";
+        if (discriminant > 1)
+        {
+            float x1 = (-b + (std::sqrt(discriminant))) / (2 * a);
+            float x2 = (-b - (std::sqrt(discriminant))) / (2 * a);
+            std::cout << "Root 1, 2: " << x1 << ", " << x2 << std::endl;
+        }
+        else if (discriminant == 0)
+        {
+            float x = -b / (2 * a);
+            std::cout << "Root: " << x << std::endl;
+        }
+        else
+        {
+            std::cout << "Complex scenario is not supported!" << std::endl;
+        }
+    }
 }
