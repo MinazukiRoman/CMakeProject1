2423b282a0b8e3bdbee66608ee97927503f1d2b2
roman_seredniov <serednv@mail.ru>
2024-12-12 15:33:51 +0300

    11.2

--- CMakeProject1/Test 2.cpp
+++ CMakeProject1/Test 2.cpp
@@ -3,41 +3,76 @@
 
 using namespace std;
 
-bool leftPart(int middle, string userEmail) {
-	int counter = 0;
-	for (int i = 0; i < middle; i++) {
-		if (!(userEmail[i] == '!' || userEmail[i] == '?')
-			&& !(userEmail[i] >= '#' && userEmail[i] <= char(39))
-			&& !(userEmail[i] >= '*' && userEmail[i] <= '+')
-			&& !(userEmail[i] >= '-' && userEmail[i] <= '9')
-			&& !(userEmail[i] >= '^' && userEmail[i] <= '~')) {
-			return false;
-		}
-		counter++;
-	}
-	if (counter < 1 || counter > 64) {
+bool leftPart(string firstHalf) {
+	string correctSymbol = "abcdefghijklmnopqrstuvwxyz0123456789!#$%&'*+-/=?^_`{|}~.";
+	if (firstHalf.length() < 1 || firstHalf.length() > 64) {
 		return false;
 	}
-}
-
-bool rightPart(int middle, string userEmail) {
-
-}
-
-int main() {
-	string userEmail;
-	cout << "Input email: ";
-	getline(cin, userEmail);
-	int middle;
-
-	for (int i = 0; i < userEmail.length(); i++) {
-		if (userEmail[i] == '@') {
-			middle = i;
+	for (int i = 0; i < firstHalf.length(); i++) {
+		if ((firstHalf[0] == '.') || (firstHalf[i] == '.' && (firstHalf[i + 1] == firstHalf[i]))) {
+			return false;
+		}
+		bool find = false;
+		for (int j = 0; j < correctSymbol.length(); j++) {
+			if (firstHalf[i] == correctSymbol[j]) {
+				find = true;
+				break;
+			}
+		}
+		if (find == false) {
+			return false;
 			break;
 		}
 	}
-	if (leftPart(middle, userEmail) && rightPart(middle, userEmail)) {
-		cout << "ok";
+	return true;
+}
+
+bool rightPart(string secondHalf) {
+	string correctSymbol = "abcdefghijklmnopqrstuvwxyz0123456789-.";
+	if (secondHalf.length() < 1 || secondHalf.length() > 63) {
+		return false;
+	}
+	for (int i = 0; i < secondHalf.length(); i++) {
+		if (secondHalf[secondHalf.length() - 1] == '.' || (secondHalf[i] == '.' && (secondHalf[i + 1] == secondHalf[i]))) {
+			return false;
+		}
+		bool find = false;
+		for (int j = 0; j < correctSymbol.length(); j++) {
+			if (secondHalf[i] == correctSymbol[j]) {
+				find = true;
+				break;
+			}
+		}
+		if (find == false) {
+			return false;
+			break;
+		}
+	}
+	return true;
+}
+
+int main() {
+
+	string firstHalf;
+	string secondHalf;
+	bool find = false;
+
+	string userEmail;
+	cout << "Input email: ";
+	getline(cin, userEmail);
+
+	for (int i = 0; i < userEmail.length(); i++) {
+		if (userEmail[i] == '@') {
+			find = true;
+			i++;
+		}
+		if (find) {
+			secondHalf += userEmail[i];
+		}
+		else {
+			firstHalf += userEmail[i];
+		}
 	}
 
+	cout << string((leftPart(firstHalf) && rightPart(secondHalf)) ? "Yes" : "No");
 }
